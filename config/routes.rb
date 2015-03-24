a314547a4f6d079dfb14c827acf523f59d2d544d
Brock Cooper <brockcooper@gmail.com>
2015-03-24 07:27:24 -0600

    Finish user show page and Gravator support

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
+  resources :users, only: [:show], :as => :user
   get 'welcome/index'
   get 'welcome/about'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312115927) do
+ActiveRecord::Schema.define(version: 20150323133554) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "todos", ["user_id"], name: "index_todos_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
