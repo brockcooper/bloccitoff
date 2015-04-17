82ad2b57ed45aeaf8f527aa14dc74611c5c97bd5
Brock Cooper <brockcooper@gmail.com>
2015-04-17 06:49:47 -0600

    Add user creation

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,4 +6,20 @@ class Api::UsersController < ApiController
     render json: @users, each_serializer: UserSerializer
   end
 
+  def create
+    # Example request:
+    # curl -u user@example.com:your-password -d "user[username]=brockcooper" -d "user[email]=u0821793@utah.edu" -d "user[password]=helloworld" http://localhost:3000/api/users/
+    user = User.new(user_params)
+    if user.save
+      render json: user.to_json
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,18 @@ Rails.application.routes.draw do
   end
 
   namespace :api, defaults: { format: :json} do
-    resources :users
+    resources :users do 
+      resources :lists
+    end
+
+    resources :lists, only: [] do
+      resources :items, only: [:create]
+    end
+
+    resources :items, only: [:destroy]
   end
+
+
 
   get 'welcome/index'
   get 'welcome/about'
